80c3b378e21fcc3443e6147e0fe767b48d71204c
Steve Roettger <steve@steveroettger.com>
2011-10-19 16:30:58 -0400

    added a users factory and tests for the show page

--- Gemfile
+++ Gemfile
@@ -11,4 +11,5 @@ end
 group :test do
   gem 'rspec', '2.0.0.beta.18'
   gem 'spork', '0.9.0.rc8'
+  gem 'factory_girl_rails', '1.0'
 end
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+	user.name                  "Steve Roettger"
+	user.email                 "steve@steveroettger.com"
+	user.password              "foobar"
+	user.password_confirmation "foobar"
+end          
